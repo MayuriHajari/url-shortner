9d71e584ad7f4eed1a0935cd4dd622b5fd6bc47a
Mayuri Hajari <mayu.hajari@gmail.com>
2026-05-16 20:13:22 +0530

    Added invalid short code test

--- test_main.py
+++ test_main.py
@@ -10,6 +10,7 @@ client = TestClient(app)
 
 def test_shorten_and_redirect():
 
+    # Step 1: Shorten URL
     response = client.post(
         "/shorten",
         json={
@@ -21,13 +22,17 @@ def test_shorten_and_redirect():
 
     data = response.json()
 
+    assert "short_code" in data
+
     short_code = data["short_code"]
 
+    # Step 2: Redirect using short code
     redirect_response = client.get(
         f"/redirect?code={short_code}",
         follow_redirects=False
     )
 
+    # Step 3: Validate redirect
     assert redirect_response.status_code == 302
 
     assert (
@@ -72,5 +77,19 @@ def test_duplicate_url_returns_same_short_code():
 
     short_code_2 = response2.json()["short_code"]
 
-    # Both short codes should match
+    # Same short code should be returned
     assert short_code_1 == short_code_2
+
+
+def test_non_existent_short_code():
+
+    response = client.get(
+        "/redirect?code=invalid123",
+        follow_redirects=False
+    )
+
+    assert response.status_code == 404
+
+    assert response.json() == {
+        "detail": "Short code not found."
+    }
